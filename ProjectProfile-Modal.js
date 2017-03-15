96e3b4fda91641cc4315424ec13d2b167b5bfefa
Hannah <wangotangoo8@yahoo.com>
2017-03-15 12:50:54 -0400

    Update ProjectProfile-Modal.js

--- ProjectProfile-Modal.js
+++ ProjectProfile-Modal.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 	$('.modal').modal();
 	$('.tooltipped').tooltip({delay: 50});
+	var config = {
+    apiKey: "AIzaSyBAImpIGb9JxO_FYM3jWj4lwNojBy0wCmc",
+    authDomain: "cycology-bf692.firebaseapp.com",
+    databaseURL: "https://cycology-bf692.firebaseio.com",
+    storageBucket: "cycology-bf692.appspot.com",
+    messagingSenderId: "406373055263"
+  };
 });
 
