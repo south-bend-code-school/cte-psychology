454d663e856dcd46345a51766d1163bea5fca122
Hannah <wangotangoo8@yahoo.com>
2017-03-15 13:58:18 -0400

    Update ProjectProfile-Modal.js

--- ProjectProfile-Modal.js
+++ ProjectProfile-Modal.js
@@ -20,5 +20,21 @@ $(document).ready(function(){
 		var projtitle = $('title_of_project').val();
 		var description = $('descriptionmodal').val();
 		var instructions = $('instructionsmodal').val();
+	
+			var project={
+			first_name : fname,
+			last_name : lname,
+			e-mail : email,
+			title : projtitle,
+			description : description,
+			instructions : instructions,
+			};
+	
+	
+			var newProjectKey = firebase.database().ref().child('Project').push().key;
+			var updates = {};
+			updates ['/Project/' + newProjectKey] = project;
+	firebase.storage().ref().child('images/projects/' + newProjectKey).put($('#photo')[0].files[0]).then(function(snapshot){return firebase.database.ref().update(updates).then(function(){
+		window.location.replace('Community Projects.html');
 });
 
